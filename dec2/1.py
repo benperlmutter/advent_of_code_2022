d7b63f2be312e0842fd0cff1fee36f7b52d182fb
benperlmutter <benperlmutter@gmail.com>
2022-12-05 08:46:26 -0800

    dec2 1 solution

--- dec2/1.py
+++ dec2/1.py
@@ -3,72 +3,35 @@
 file1 = open('input.txt', 'r')
 lines = file1.readlines()
 
-abc_s = "abcdefghijklmnopqrstuvwxyz"
 
-scoreDict = {}
+solutionDict = {
+	"A": {
+	"X":1+3,
+	"Y":2+6,
+	"Z":3+0
+	},
+	"B": {
+	"Y":2+3,
+	"Z":3+6,
+	"X":1+0
+	},
+	"C": {
+	"Z":3+3,
+	"X":1+6,
+	"Y":2+0
+	}
+}
 
-for char in range(len(abc_s)):
-	scoreDict[abc_s[char]] = char+1
-	# print(scoreDict)
-
-count = 0
+score = 0
 for line in lines:
-	dict1 = {}
 	thisLine = line.strip()
-	halfLength = int(len(thisLine))/2
-	for char in thisLine[0:int(halfLength)]:
-		dict1[char] = True
+	splitLine = thisLine.split()
+	print(thisLine)
+	them = splitLine[0]
+	me = splitLine[1]
+	score += solutionDict[them][me]
+	# print("{} vs {}".format(them, me))
+	# print("{} and {}".format(solutionDict[them][me], score))
+	# print("Line {}: {}".format(count, line.strip()))
 
-	shared = ""
-
-	for char in thisLine[int(halfLength):len(thisLine)]:
-		if dict1.get(char) == True:
-			dict1[char] = False
-			shared = char
-			count += scoreDict[shared.lower()]
-			thisScore = scoreDict[shared.lower()]
-			if shared == shared.upper():
-				count += 26 
-				thisScore += 26
-			print("the letter is {} and the score is {}".format(shared,thisScore))
-
-print(count)
-
-
-# solutionDict = {
-# 	"A": {
-# 	"X":3,
-# 	"Y":6,
-# 	"Z":0
-# 	},
-# 	"B": {
-# 	"Y":3,
-# 	"Z":6,
-# 	"X":0
-# 	},
-# 	"C": {
-# 	"Z":3,
-# 	"X":6,
-# 	"Y":0
-# 	}
-# }
-
-# score = 0
-# for line in lines:
-# 	thisLine = line.strip()
-# 	splitLine = thisLine.split()
-# 	them = splitLine[0]
-# 	me = splitLine[1]
-# 	if me == "X":
-# 		score += 1
-# 	elif me == "Y":
-# 		score += 2
-# 	elif me == "Z":
-# 		score += 3
-
-# 	score += solutionDict[them][me]
-# 	# print("{} vs {}".format(them, me))
-# 	print("{} and {}".format(solutionDict[them][me], score))
-# 	# print("Line {}: {}".format(count, line.strip()))
-
-# print(score)
+print(score)
