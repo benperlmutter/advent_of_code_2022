470a7e580bada163eaaa01936c05e9ff41638aef
benperlmutter <benperlmutter@gmail.com>
2022-12-05 14:32:46 -0800

    dec4 solution 2 complete

--- dec4/1.py
+++ dec4/1.py
@@ -1,74 +1,72 @@
 #dec1 1
 
+# file1 = open('sample_input.txt', 'r')
 file1 = open('input.txt', 'r')
 lines = file1.readlines()
-
-abc_s = "abcdefghijklmnopqrstuvwxyz"
-
-scoreDict = {}
-
-for char in range(len(abc_s)):
-	scoreDict[abc_s[char]] = char+1
-	# print(scoreDict)
-
 count = 0
+newCount = 0
+lineNum = 0
+oldStr = ""
+newStr = ""
+newerCount = 0
 for line in lines:
-	dict1 = {}
+	lineNum +=1
 	thisLine = line.strip()
-	halfLength = int(len(thisLine))/2
-	for char in thisLine[0:int(halfLength)]:
-		dict1[char] = True
+	elves = thisLine.split(',')
+	elf1 = elves[0].split('-')
+	elf2 = elves[1].split('-')
+	elf11 = int(elf1[0])
+	elf12 = int(elf1[1])
+	elf21 = int(elf2[0])
+	elf22 = int(elf2[1])
+	elf1Str = ","
+	elf2Str = ","
+	for i in range(elf11, elf12+1):
+		elf1Str += str(i)+","
+	for g in range(elf21, elf22+1):
+		elf2Str += str(g)+","
 
-	shared = ""
+	# print("elf1 {}".format(elf1Str))
+	# print("elf2 {}".format(elf2Str))
 
-	for char in thisLine[int(halfLength):len(thisLine)]:
-		if dict1.get(char) == True:
-			dict1[char] = False
-			shared = char
-			count += scoreDict[shared.lower()]
-			thisScore = scoreDict[shared.lower()]
-			if shared == shared.upper():
-				count += 26 
-				thisScore += 26
-			print("the letter is {} and the score is {}".format(shared,thisScore))
+	if elf1Str in elf2Str:
+		oldStr += "elf1 in elf2 {} {}".format(elf1Str, elf2Str)+'\n'
+		if lineNum == 991:
+			print("elf1 in elf2 {} {}".format(elf1Str, elf2Str))
+		# print("elf1 in elf2 {} {}".format(elf1Str, elf2Str))
+		count +=1
+		print(lineNum)
+	elif elf2Str in elf1Str:
+		oldStr += "elf2 in elf1 {} {}".format(elf2Str, elf1Str)+'\n'
+		if lineNum == 991:
+			print("elf1 in elf2 {} {}".format(elf1Str, elf2Str))
+		# print("elf2 in elf1 {} {}".format(elf2Str, elf1Str))
+		count +=1
+		print(lineNum)
 
+	if elf1Str.find(elf2Str) > -1:
+		newStr += "elf2 in elf1 {} {}".format(elf2Str, elf1Str)+'\n'
+		newCount += 1
+		# print(lineNum)
+	elif elf2Str.find(elf1Str) > -1:
+		newStr += "elf1 in elf2 {} {}".format(elf1Str, elf2Str)+'\n'
+		newCount += 1
+		# print(lineNum)
+
+	if ((elf11 <= elf21) and (elf12 >= elf22)) or ((elf21 <= elf11) and (elf22 >= elf12)):
+		newerCount += 1
+		# print(lineNum)
+
+
+# print(newStr)
+# print("final count is {}".format(newCount))
+
+test1 = "4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,"
+test2 = "4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,"
+
+# print(test1.find(test2))
 print(count)
+print(newCount)
+print(newerCount)
 
 
-# solutionDict = {
-# 	"A": {
-# 	"X":3,
-# 	"Y":6,
-# 	"Z":0
-# 	},
-# 	"B": {
-# 	"Y":3,
-# 	"Z":6,
-# 	"X":0
-# 	},
-# 	"C": {
-# 	"Z":3,
-# 	"X":6,
-# 	"Y":0
-# 	}
-# }
-
-# score = 0
-# for line in lines:
-# 	thisLine = line.strip()
-# 	splitLine = thisLine.split()
-# 	them = splitLine[0]
-# 	me = splitLine[1]
-# 	if me == "X":
-# 		score += 1
-# 	elif me == "Y":
-# 		score += 2
-# 	elif me == "Z":
-# 		score += 3
-
-# 	score += solutionDict[them][me]
-# 	# print("{} vs {}".format(them, me))
-# 	print("{} and {}".format(solutionDict[them][me], score))
-# 	# print("Line {}: {}".format(count, line.strip()))
-
-# print(score)
--- dec4/2.py
+++ dec4/2.py
@@ -1,61 +1,72 @@
 #dec1 1
 
+# file1 = open('sample_input.txt', 'r')
 file1 = open('input.txt', 'r')
 lines = file1.readlines()
-
-abc_s = "abcdefghijklmnopqrstuvwxyz"
-
-scoreDict = {}
-dict1 = {}
-for char in range(len(abc_s)):
-	scoreDict[abc_s[char]] = char+1
-	dict1[abc_s[char]] = 0
-	dict1[abc_s[char].upper()] = 0
-	# print(scoreDict)
-
 count = 0
-thisScore = 0
-setC = 1
-
+newCount = 0
+lineNum = 0
+oldStr = ""
+newStr = ""
+newerCount = 0
 for line in lines:
+	lineNum +=1
 	thisLine = line.strip()
-	if setC % 3 == 1:
-		for char in thisLine:
-			dict1[char] = 1
-	elif setC % 3 == 2:
-		for char in thisLine:
-			if dict1.get(char) == True:
-				dict1[char] += 1
-				# print(dict1[char])
-	elif setC % 3 == 0:
-		print(thisLine)
-		for char in thisLine:
-			# print("current set is {} and dict looks like {}".format(setC, dict1))
-			print(dict1.get("r") == True)
+	elves = thisLine.split(',')
+	elf1 = elves[0].split('-')
+	elf2 = elves[1].split('-')
+	elf11 = int(elf1[0])
+	elf12 = int(elf1[1])
+	elf21 = int(elf2[0])
+	elf22 = int(elf2[1])
+	elf1Str = ","
+	elf2Str = ","
+	for i in range(elf11, elf12+1):
+		elf1Str += str(i)+","
+	for g in range(elf21, elf22+1):
+		elf2Str += str(g)+","
 
-			if dict1[char] > 1:
-				# print(char)
-				# print("the CHAR {}".format(char))
+	# print("elf1 {}".format(elf1Str))
+	# print("elf2 {}".format(elf2Str))
 
-				if dict1[char] > 1:
-					shared = char
-					count += scoreDict[shared.lower()]
-					thisScore = scoreDict[shared.lower()]
-					if shared == shared.upper():
-						count += 26 
-						thisScore += 26
-					for char in range(len(abc_s)):
-						dict1[abc_s[char]] = 0
-						dict1[abc_s[char].upper()] = 0
-				# print("count is {}".format(count))
-				# print(thisScore)
-	# print("current set is {} and dict looks like {}".format(setC, dict1))
+	if elf1Str in elf2Str:
+		oldStr += "elf1 in elf2 {} {}".format(elf1Str, elf2Str)+'\n'
+		if lineNum == 991:
+			print("elf1 in elf2 {} {}".format(elf1Str, elf2Str))
+		# print("elf1 in elf2 {} {}".format(elf1Str, elf2Str))
+		count +=1
+		print(lineNum)
+	elif elf2Str in elf1Str:
+		oldStr += "elf2 in elf1 {} {}".format(elf2Str, elf1Str)+'\n'
+		if lineNum == 991:
+			print("elf1 in elf2 {} {}".format(elf1Str, elf2Str))
+		# print("elf2 in elf1 {} {}".format(elf2Str, elf1Str))
+		count +=1
+		print(lineNum)
 
-	setC += 1
+	if elf1Str.find(elf2Str) > -1:
+		newStr += "elf2 in elf1 {} {}".format(elf2Str, elf1Str)+'\n'
+		newCount += 1
+		# print(lineNum)
+	elif elf2Str.find(elf1Str) > -1:
+		newStr += "elf1 in elf2 {} {}".format(elf1Str, elf2Str)+'\n'
+		newCount += 1
+		# print(lineNum)
 
+	if ((elf11 <= elf21) and (elf12 >= elf21)) or ((elf11 <= elf22) and (elf12 >= elf22)) or ((elf21 <= elf11) and (elf22 >= elf11)) or ((elf21 <= elf12) and (elf22 >= elf12)):
+		newerCount += 1
+		# print(lineNum)
+
+
+# print(newStr)
+# print("final count is {}".format(newCount))
+
+test1 = "4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,"
+test2 = "4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,"
+
+# print(test1.find(test2))
 print(count)
-
-
-
+print(newCount)
+print(newerCount)
 
 
