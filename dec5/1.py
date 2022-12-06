19a8b3e1211d892489a535869b8cb9679996617c
benperlmutter <benperlmutter@gmail.com>
2022-12-06 15:04:38 -0800

    dec5 solution update but not complete

--- dec5/1.py
+++ dec5/1.py
@@ -4,6 +4,8 @@
 file1 = open('input.txt', 'r')
 lines = file1.readlines()
 
+lineCounter = 1
+
 counter = ""
 stackLines = []
 moveLines = []
@@ -21,6 +23,7 @@ for i in range(int(stackLines[-2][-2])):
 	realStack.append([])
 
 for row in stackLines[::-1]:
+	lineCounter += 1
 	# print(row)
 	counter = 0
 	spaceCounter = 0
@@ -34,12 +37,47 @@ for row in stackLines[::-1]:
 				counter += 1
 				spaceCounter = 0
 
-print(realStack)
+# print(realStack[0].pop())
 
+# print(realStack)
+i = 1
+for x in realStack:
+	print("{} {}".format(i,x))
+	i+=1
 
+count = 1
 
-# print(stackLines)
-# print(moveLines)
+for line in moveLines:
+	lineCounter += 1
+	# print(int(lines[1]))
+	# print(line)
+	numMoves = int(line[1])
+	start = int(line[3])
+	end = line[5]
+	splitEnd = end.split('\n')
+	end = int(splitEnd[0])
+	# print("{} {} {}".format(numMoves, start, end))
+	# print(lineCounter)
+	# for x in realStack:
+		# print(x)
+	for move in range(numMoves):
+		realStack[end-1].append(realStack[start-1].pop())
+		# if count > 30 and count <= 37:
+		# 	print("in while")
+		# 	print("{} {} {}".format(numMoves, start, end))
+		# 	print(lineCounter)
+		# 	i = 1
+		# 	for x in realStack:
+		# 		print("{} {}".format(i,x))
+		# 		i += 1
+	count += 1
+		# print("move")
+
+# print(realStack)
+
+# for stack in realStack:
+	# print(stack.pop())
+# print(moveLines[-2])
 	# n = 3
 	# if counter == 1:
 	# 	for i in range (0, len(line), n):
