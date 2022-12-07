a643093bd84d9e751f07cd4b1e581cd777f2f2d2
benperlmutter <benperlmutter@gmail.com>
2022-12-07 06:08:00 -0800

    dec6 solution2 complete

--- dec6/2.py
+++ dec6/2.py
@@ -10,26 +10,46 @@ char2 = ""
 char3 = ""
 char4 = ""
 counter = 1
+finalCounter = 0
+finalStr = ""
 for line in lines:
+	counter = 1
+	charStr = ""
+	firstTime = True
 	for char in line:
-		if (char1 not in char2+char3+char4) and (char2 not in char1+char3+char4) and (char3 not in char1+char2+char4) and (char4 not in char1+char2+char3):
-			print("{}{}{}{} and counter is {}".format(char1,char2,char3,char4,counter-1))
-		elif counter == 1:
-			char1 = char
-			counter += 1
-		elif counter == 2:
-			char2 = char
-			counter += 1
-		elif counter == 3:
-			char3 = char
-			counter += 1
-		elif counter == 4:
-			char4 = char
-			counter += 1
+		if counter <= 14:
+			charStr += char
 		else:
-			char1 = char2
-			char2 = char3
-			char3 = char4
-			char4 = char
-			counter += 1
+			charStr = charStr[1:]+char
+			# print("{} is {}".format(counter, charStr))
+
+		if len(charStr) == 14:
+			dict1 = {}
+			flag = True
+			for c in charStr:
+				if dict1.get(c):
+					dict1[c] += 1
+				else:
+					dict1[c] = 1
+			for c in charStr:
+				if dict1[c] > 1:
+					flag = False
+			if flag and firstTime:
+				print('in here')
+				finalCounter = counter
+				finalStr = charStr
+				firstTime = False
+
+		counter+=1
+				# print("we have a winner at {}".format(counter))
+	print("we have a winner with {} at {}".format(finalStr, finalCounter))
+
+
+
+
+		# dict1 = {}
+		# for c in charStr:
+
+
+		
 
