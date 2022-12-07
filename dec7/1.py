bf57b99b1bac30cabf040d9967dfba5f499164b5
benperlmutter <benperlmutter@gmail.com>
2022-12-07 11:50:02 -0800

    dec7 solution1 complete

--- dec7/1.py
+++ dec7/1.py
@@ -1,24 +1,66 @@
 #dec1 1
 
-file1 = open('sample_input.txt', 'r')
-# file1 = open('input.txt', 'r')
+def sumDir(dirDict,key):
+	a = dirDict[key]
+	acc = 0
+	if a[0] == []:
+		collector = 0
+		for num in a[1]:
+			collector += num
+		return collector
+	else:
+		for d in a[0]:
+			acc += sumDir(dirDict,d)
+	for n in a[1]:
+		acc += n
+
+	return acc
+
+
+# file1 = open('simple_input.txt', 'r')
+# file1 = open('sample_input.txt', 'r')
+file1 = open('input.txt', 'r')
 lines = file1.readlines()
 
-def my_function():
-	print('this is the function')
+dirDict = {"/":[[],[]]}
 
-def my_recurse(varName):
-	if varName == 0:
-		return 1
-	else:
-		return varName * my_recurse(varName-1)
-
-def processLine(el):
-	size = len(el)
-	if el[0] == "$":
-		print(el[0])
-
+currDir = []
+acc = 0
 for line in lines:
-	splLine = line.split(" ")
+	el = line.split(" ")
+	if el[0] == "$":
+		acc = 0
+		if el[1] == "cd":
+			if el[2] == "..\n":
+				currDir.pop()
+			else:
+				currDir.append(el[2].split("\n")[0])
+				strDir = "#".join(currDir)
+				# print(strDir)
+				if dirDict.get(strDir) == None:
+					# print('here')
+					dirDict[strDir] = [[],[]]
+	elif el[0] == "dir":
+		stringCurrDir = "#".join(currDir)
+		newDir = currDir.copy()
+		newDir.append(el[1].split("\n")[0])
+		strDir = "#".join(newDir)
+		dirDict[stringCurrDir][0].append(strDir)
+		# print(strDir)
+	else:
+		# print(currDir)
+		strDir = "#".join(currDir)
+		dirDict[strDir][1].append(int(el[0]))
+
 	# print(splLine)
-	processLine(splLine)
+# print(dirDict)
+
+sumTotal = 0
+
+for key in dirDict:
+	# print("{} dir has {} bytes".format(key, sumDir(dirDict,key)))
+	tot = sumDir(dirDict,key)
+	if tot <= 100000:
+		sumTotal += tot
+
+print(sumTotal)
---
+++ dec7/simple.py
@@ -0,0 +1,28 @@
+file1 = open('simple_input.txt', 'r')
+
+lines = file1.readlines()
+
+def sumDir(a):
+	if a[0] == []:
+		collector = 0
+		for num in a[1]:
+			collector += num
+			# print(collector)
+		return collector
+	else:
+		acc = 0
+		recursiveNum = 0
+		for num in a[1]:
+			acc += num
+			print(acc)
+		for d in a[0]:
+			recursiveNum += sumDir(d)
+			print(recursiveNum)
+
+		return acc + recursiveNum
+
+b = [[],[3]]
+d = [[],[6,4]]
+dirList = [[b,d],[1,2,4]]
+
+print(sumDir(dirList))
