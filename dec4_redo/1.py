1822967ad7219b5f650e8cf32b52e7d6363c7912
benperlmutter <benperlmutter@gmail.com>
2022-12-07 14:49:29 -0800

    dec4_redo solution 1 with python set functions

---
+++ dec4_redo/1.py
@@ -0,0 +1,26 @@
+#dec1 1
+
+# file1 = open('sample_input.txt', 'r')
+file1 = open('input.txt', 'r')
+lines = file1.readlines()
+
+counter = 0
+
+for line in lines:
+	line = line.split("\n")[0]
+	elfLine = line.split(",")
+	# print(elfLine)
+	elf1 = elfLine[0].split("-")
+	elf2 = elfLine[1].split("-")
+	# print(elf1)
+	# print(elf2)
+	elf1List = []
+	elf1List.extend(range(int(elf1[0]), int(elf1[1])+1))
+	elf1Set = set(elf1List)
+	elf2List = []
+	elf2List.extend(range(int(elf2[0]), int(elf2[1])+1))
+	elf2Set = set(elf2List)
+	if elf1Set.issubset(elf2Set) or elf2Set.issubset(elf1Set):
+		counter += 1
+
+print(counter)
