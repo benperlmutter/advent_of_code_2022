3701a3a5b60554d869ef547f527b4ea915e8bc2b
benperlmutter <benperlmutter@gmail.com>
2022-12-14 15:32:41 -0800

    dec11 inputs and template set

---
+++ dec11/1.py
@@ -0,0 +1,6 @@
+#dec11 1
+
+# file1 = open('sample_input.txt', 'r')
+file1 = open('input.txt', 'r')
+lines = file1.readlines()
+
---
+++ dec11/2.py
@@ -0,0 +1,6 @@
+#dec11 2
+
+# file1 = open('sample_input.txt', 'r')
+file1 = open('input.txt', 'r')
+lines = file1.readlines()
+
