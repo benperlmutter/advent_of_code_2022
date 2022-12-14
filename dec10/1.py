ec8d3a1072ac06415fee28229c4a08b19b4e1bff
benperlmutter <benperlmutter@gmail.com>
2022-12-14 08:44:53 -0800

    dec10 solution 1 complete

--- dec10/1.py
+++ dec10/1.py
@@ -1,75 +1,41 @@
 #dec9 1
 
+
+
+
+
+
 # file1 = open('sample_input.txt', 'r')
 file1 = open('input.txt', 'r')
 lines = file1.readlines()
 
-def moveRope(coord, direction, num, h, t, moveDict):
-	for i in range(num):
-		# print(moveDict)
-		if coord == "x":
-			if direction == "add":
-				h[0] += 1
-				if abs(h[0] - t[0]) > 1:
-					if abs(h[1] - t[1]) == 1:
-						t[1] = h[1]
-					t[0] += 1
-					moveDict.setdefault(""+str(t[0])+","+str(t[1]), 0)
-					moveDict[""+str(t[0])+","+str(t[1])] += 1
+cycle = 0
+x = 1
+total = 0
 
-			else:
-				h[0] -= 1
-				if abs(h[0] - t[0]) > 1:
-					if abs(h[1] - t[1]) == 1:
-						t[1] = h[1]
-					t[0] -= 1
-					moveDict.setdefault(""+str(t[0])+","+str(t[1]), 0)
-					moveDict[""+str(t[0])+","+str(t[1])] += 1
-
-		else:
-			if direction == "add":
-				h[1] += 1
-				if abs(h[1] - t[1]) > 1:
-					if abs(h[0] - t[0]) == 1:
-						t[0] = h[0]
-					t[1] += 1
-					moveDict.setdefault(""+str(t[0])+","+str(t[1]), 0)
-					moveDict[""+str(t[0])+","+str(t[1])] += 1
-			else:
-				h[1] -= 1
-				if abs(h[1] - t[1]) > 1:
-					if abs(h[0] - t[0]) == 1:
-						t[0] = h[0]
-					t[1] -= 1
-					moveDict.setdefault(""+str(t[0])+","+str(t[1]), 0)
-					moveDict[""+str(t[0])+","+str(t[1])] += 1
-
-def def_value():
-	return 0
-
-
-head = [0,0]
-tail = [0,0]
-moveDict = {"0,0": 1}
+cycles = [20,60,100,140,180,220]
 
 for line in lines:
-	l = line.split(" ")
-	direction = l[0]
-	number = int(l[1])
-	if direction == "R":
-		# print("add to x coord")
-		moveRope("x", "add", number, head, tail, moveDict)
-	elif direction == "U":
-		# print("add to y coord")
-		moveRope("y", "add", number, head, tail, moveDict)
-	elif direction == "D":
-		# print("subtract from y coord")
-		moveRope("y", "subtract", number, head, tail, moveDict)
-	else:
-		# print("subtract from x coord")
-		moveRope("x", "subtract", number, head, tail, moveDict)
+	l = line.split()
+	# print(l)
+	if len(l) <2:
+		cycle +=1
+		if cycle in cycles:
+			print("cycle is {} and x is {} and combo is {}".format(cycle,x,cycle*x))
+			total += cycle*x
 
-	# print(direction)
-	# print(number)
-print(moveDict)
-print(len(moveDict))
+	else:
+		cycle += 1
+		if cycle in cycles:
+			print("cycle is {} and x is {} and combo is {}".format(cycle,x,cycle*x))
+			total += cycle*x
+
+		
+		cycle += 1
+		if cycle in cycles:
+			print("cycle is {} and x is {} and combo is {}".format(cycle,x,cycle*x))
+			total += cycle*x
+
+		x += int(l[1])
+
+print(total)
