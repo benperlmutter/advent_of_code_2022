414ed5a3af1459e67b02c708cc008bffba018e7a
benperlmutter <benperlmutter@gmail.com>
2022-12-14 13:15:14 -0800

    dec10 sol2 complete

--- dec10/2.py
+++ dec10/2.py
@@ -4,33 +4,105 @@
 file1 = open('input.txt', 'r')
 lines = file1.readlines()
 
-cycle = 0
+c = 0
 x = 1
 total = 0
 
-cycles = [20,60,100,140,180,220]
+gridSize = 40
+
+a = []
+b = []
+
+p=""
 
 for line in lines:
 	l = line.split()
-	# print(l)
-	if len(l) <2:
-		cycle +=1
-		if cycle in cycles:
-			print("cycle is {} and x is {} and combo is {}".format(cycle,x,cycle*x))
-			total += cycle*x
+	a.append(l)
 
-	else:
-		cycle += 1
-		if cycle in cycles:
-			print("cycle is {} and x is {} and combo is {}".format(cycle,x,cycle*x))
-			total += cycle*x
+for i in range(len(a)):
+	b.append(a.pop())
 
-		
-		cycle += 1
-		if cycle in cycles:
-			print("cycle is {} and x is {} and combo is {}".format(cycle,x,cycle*x))
-			total += cycle*x
+count = 0
+# z = len(b)
+# for el in range(z):
+# 	print(el)
+# 	l = b.pop()
+# 	# print(len(l))
+# 	if len(l) < 2:
+# 		count += 1
+# 	else:
+# 		count += 2
 
+# print(count)
+
+while len(b) > 0:
+	# print(el)
+	# print('this is length of b {}'.format(len(b)))
+	flag = False
+	while (len(p) < gridSize) and (len(b) > 0):
+		l = b.pop()
+		# print(l[0])
+		if len(l) <2:
+			# count += 1
+			# print("start cycle {}".format(len(p)))
+			# print("x is {}".format(x))
+			if (x == len(p)) or (x-1 == len(p)) or (x+1 == len(p)):
+				p += "#"
+				count += 1
+			else:
+				p += "."
+				count += 1
+
+		else:
+			# count += 2
+			# print("start cycle {}".format(c))
+			# print("x is {}".format(x))
+			if (x == len(p)) or (x-1 == len(p)) or (x+1 == len(p)):
+				p += "#"
+				count += 1
+			else:
+				p += "."
+				count += 1
+
+			# print(x)
+			# print("cycle is {}".format(c))
+			# print(p)
+			
+			# print("start cycle {}".format(c))
+			if len(p) < gridSize:
+				if (x == len(p)) or (x-1 == len(p)) or (x+1 == len(p)):
+					p += "#"
+					count += 1
+				else:
+					p += "."
+					count += 1
+			else:
+				flag = True
+
+
+				# print(x)
+				# print("cycle is {}".format(c))
+				# print(p)
+
+			if flag == False:
+				x += int(l[1])
+			# print(x)
+
+		# print(c)
+	print(p)
+	# x = 1
+	p = ""
+	if flag:
+		if (x == len(p)) or (x-1 == len(p)) or (x+1 == len(p)):
+			p += "#"
+			count += 1
+		else:
+			p += "."
+			count += 1
 		x += int(l[1])
 
-print(total)
+
+print(count)
+
+# print(b)
+# print(b.pop())
